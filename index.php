de1882b50eec04e2a42beb8bce6e0cadc03b7197
zainuull <thobazainul@gmail.com>
2023-07-28 10:38:11 +0700

    menambahkan file

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
   <h1>GIT REMOTE LOCAL</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit non error voluptate exercitationem perspiciatis autem dicta quisquam, reprehenderit accusantium. Explicabo corrupti quaerat quas iusto ipsa cupiditate illum error voluptatem totam?</p>
 </body>
 </html>
